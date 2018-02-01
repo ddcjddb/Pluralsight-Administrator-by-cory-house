9fb8ec702955554121735e8465b5cb07b551c30c
ddcjddb <11126377@qq.com>
2018-02-02 00:41:35 +0800

    course-15 done with one problem  css failed to load   needed to be solved

---
+++ src/store/configureStore.prod.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from '../reducers/index';
+
+import thunk from 'redux-thunk';
+
+export default function configureStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunk)
+  );
+}
